3bb881934ed4e135640edd745a4c5bc8c7966c20
yupswing <twomorewords@gmail.com>
2017-03-17 10:41:25 +0100

    Improved user feedback + keep track of best calculation + better README.md

--- README.md
+++ README.md
@@ -1,7 +1,37 @@
 # pirand
-calculate π from random numbers
+calculate π from pairs of random numbers
 
-Happy (Pi Day)+1 2017
+MIT Licence (c) 2017 Simone Cingano
+
+Made on 3.15 2017
+
+Happy (Pi Day)+0.01 2017
+
+# Usage
+
+`python pirand.py [faces] [rolls]`
+
+
+default parameters (with output):
+```
+» python pirand.py
+500 random rolls of two d120
+progress [====================] 100.0% | current error 1.85373%
+ math.pi | 3.14159265359
+   Final | 3.19982934699 (1.8537315246% error)
+    Best | 3.14112506384 (0.0148838441% error at the roll number 147)
+```
+
+a better run (with output):
+```
+» python pirand.py 9999999999 9999999
+9,999,999 random rolls of two d9,999,999,999
+progress [====================] 100.0% | current error 0.00931%
+ math.pi | 3.14159265359
+   Final | 3.14130030793 (0.0093056515% error)
+    Best | 3.14159265202 (0.0000000501% error at the roll number 812,427)
+python pirand.py 9999999999 9999999  60,06s user 0,72s system 97% cpu 1:02,02 total
+```
 
 # Why have you done this?
 Watch this video by Matt Parker and you will understand
--- pirand.py
+++ pirand.py
@@ -17,11 +17,14 @@ import random
 from fractions import gcd
 
 
-def drawProgressBar(factor, length=20, fill="=", empty=" "):
+def drawProgressBar(factor, error):
     """Draw an updatable progress bar.
-    
+
     Thanks to Jacob Tsui http://stackoverflow.com/a/15801617/7142433
     """
+    length = 20
+    fill = "="
+    empty = " "
     sys.stdout.write("\r")
     progress = ""
     for i in range(length):
@@ -29,15 +32,35 @@ def drawProgressBar(factor, length=20, fill="=", empty=" "):
             progress += fill
         else:
             progress += empty
-    sys.stdout.write("[%s] %.2f%%" % (progress, factor * 100))
+    sys.stdout.write("progress [%s] %.1f%% | current error %.5f%% " %
+                     (progress, factor * 100, error))
     sys.stdout.flush()
 
 
+def calculatePi(dice_rolls, coprimes_counter):
+    """Calculate PI from total rolls and number of coprimes.
+
+    Returns: (calculated_pi, percentage_error)
+    """
+    # x = coprimes / total rolls
+    # pi ~= sqrt(6/x)
+    if coprimes_counter == 0:
+        return (0.0, 100.0)
+    calculated_pi = math.sqrt(
+        6.0 / (coprimes_counter / float(dice_rolls)))
+    percentage_error = abs((calculated_pi - math.pi) / math.pi * 100)
+    return calculated_pi, percentage_error
+
+
 if __name__ == "__main__":
+    calculate_best = True  # False to make it faster (no real time statistics)
+
     dice_faces = 120  # default is d120
     dice_rolls = 500  # default is 500 rolls
+
     # how many rolls gave out a pair of numbers that share only 1 as factor
     coprimes_counter = 0
+    percentage_error = 0
 
     # get the faces and rolls from arguments
     if len(sys.argv) >= 2:
@@ -48,26 +71,43 @@ if __name__ == "__main__":
     print "%s random rolls of two d%s" % (dice_rolls, dice_faces)
     # let's roll
     random.seed()
+    feedback = int(dice_rolls / 1000)
+    lowest_error_percentage = 100.0
+    lowest_error_rolls = 0
+    lowest_error_pi = 0
+    if feedback < 1:
+        feedback = 1
     for roll_index in range(dice_rolls):
         dice_one = random.randint(1, dice_faces)
         dice_two = random.randint(1, dice_faces)
         if gcd(dice_one, dice_two) == 1:
             # numbers are coprime when their only factor is 1
             coprimes_counter += 1
-        if roll_index % 10000 == 0:
-            drawProgressBar(roll_index / float(dice_rolls))
-            
-    drawProgressBar(1)
-    print "\r"
+        if calculate_best:
+            # calculate PI and percentage error on every roll
+            # (intensive but let the program to keep track of the
+            #  best calculated PI over all the rolls and show the current
+            #  error to the user)
+            calculated_pi, percentage_error = calculatePi(
+                roll_index + 1, coprimes_counter)
+            if lowest_error_percentage > percentage_error:
+                lowest_error_percentage = percentage_error
+                lowest_error_rolls = roll_index
+                lowest_error_pi = calculated_pi
+        if roll_index % feedback == 0:
+            drawProgressBar(roll_index / float(dice_rolls), percentage_error)
 
-    if dice_rolls == 0:
+    if coprimes_counter == 0:
         # this happens when there are very few rolls and we get no coprimes
+        drawProgressBar(1, percentage_error)
+        print "\r"
         print "We need to roll more to get at least 1 pair of coprimes"
     else:
-        # x = coprimes / total rolls
-        # pi ~= sqrt(6/x)
-        guessed_pi = math.sqrt(
-            6.0 / (float(coprimes_counter) / float(dice_rolls)))
-        percentage_error = abs((guessed_pi - math.pi) / math.pi * 100)
-
-        print "%s (%.5f%% error)" % (guessed_pi, percentage_error)
+        calculated_pi, percentage_error = calculatePi(
+            dice_rolls, coprimes_counter)
+        drawProgressBar(1, percentage_error)
+        print "\r"
+        print " math.pi | %.11f" % (math.pi)
+        print "   Final | %.11f (%.10f%% error)" % (calculated_pi, percentage_error)
+        if calculate_best:
+            print "    Best | %.11f (%.10f%% error at the roll number %s)" % (lowest_error_pi, lowest_error_percentage, lowest_error_rolls)
